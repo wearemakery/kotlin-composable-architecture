44192a5d66b0b1345b8f2c8447f15584d35224fb
Gyula Voros <gyulavoros87@gmail.com>
2020-06-04 15:35:37 +0200

    feat: introduce data binding integration

--- app/src/main/java/co/makery/tca/CounterActivity.kt
+++ app/src/main/java/co/makery/tca/CounterActivity.kt
@@ -1,36 +1,50 @@
 package co.makery.tca
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.whenCreated
-import kotlinx.android.synthetic.main.activity_counter.*
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import co.makery.tca.databinding.ActivityCounterBinding
 import kotlinx.coroutines.launch
 
-class CounterActivity : AppCompatActivity() {
+class CounterActivityViewModel : ViewModel() {
+
+    val state: MutableLiveData<CounterState> = MutableLiveData()
+
     private lateinit var scopedStore: Store<CounterState, CounterAction>
 
     init {
-        lifecycleScope.launch {
-            whenCreated {
-                scopedStore = TCApp.store.scope(
-                    toLocalState = { globalState -> globalState.counterState },
-                    fromLocalAction = { localAction -> AppAction.Counter(localAction) }
-                )
-                scopedStore.observe { state ->
-                    println("[${Thread.currentThread().name}] [scoped store] state=$state")
-                    counterTitle.text = "$state"
-                }
+        viewModelScope.launch {
+            scopedStore = TCApp.store.scope(
+                toLocalState = AppState.counterState,
+                fromLocalAction = CounterAction.prism
+            )
+            scopedStore.observe { newState ->
+                println("[${Thread.currentThread().name}] [scoped store] state=$newState")
+                state.value = newState
             }
         }
     }
 
+    fun onIncrementTap() {
+        scopedStore.send(CounterAction.Increment)
+    }
+}
+
+class CounterActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<CounterActivityViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
 
-        counterIncrementButton.setOnClickListener {
-            scopedStore.send(CounterAction.Increment)
-        }
+        val binding =
+            DataBindingUtil.setContentView<ActivityCounterBinding>(this, R.layout.activity_counter)
+
+        binding.lifecycleOwner = this
+        binding.vm = viewModel
     }
 }
--- app/src/main/java/co/makery/tca/MainActivity.kt
+++ app/src/main/java/co/makery/tca/MainActivity.kt
@@ -2,22 +2,46 @@ package co.makery.tca
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewModelScope
+import co.makery.tca.databinding.ActivityMainBinding
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+class MainActivityViewModel : ViewModel() {
+    val appState: MutableLiveData<AppState> = MutableLiveData()
+
+    init {
+        viewModelScope.launch {
+            TCApp.store.observe { newState ->
+                appState.value = newState
+            }
+        }
+    }
+}
+
 class MainActivity : AppCompatActivity() {
-    private val store = TCApp.store
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+
+        val viewModel by viewModels<MainActivityViewModel>()
+        binding.vm = viewModel
+
         startActivity(Intent(baseContext, CounterActivity::class.java))
 
         lifecycleScope.launch {
             delay(5000L)
-            store.send(AppAction.Counter(CounterAction.Increment))
+            TCApp.store.send(AppAction.Counter(CounterAction.Increment))
         }
     }
 }
