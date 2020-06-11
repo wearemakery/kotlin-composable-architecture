dff5021fbc79ae956814ecde52f044d1779a2731
Gyula Voros <gyulavoros87@gmail.com>
2020-06-11 09:23:04 +0200

    refact: clean up types a bit

--- composable-architecture/src/main/kotlin/composablearchitecture/Reducer.kt
+++ composable-architecture/src/main/kotlin/composablearchitecture/Reducer.kt
@@ -3,26 +3,23 @@ package composablearchitecture
 import arrow.optics.Lens
 import arrow.optics.Prism
 
-data class ReducerResult<out State, Action>(val state: State, val effect: Effect<Action>)
+data class Result<out State, Action>(val state: State, val effect: Effect<Action>)
 
-typealias ReducerFn<State, Action, Environment> = (State, Action, Environment) -> ReducerResult<State, Action>
-typealias ReducerNoEnvFn<State, Action> = (State, Action) -> ReducerResult<State, Action>
-
-class Reducer<State, Action, Environment>(
-    val reducer: ReducerFn<State, Action, Environment>
+class Reducer<State, Action, Environment> private constructor(
+    val reducer: (State, Action, Environment) -> Result<State, Action>
 ) {
     companion object {
         operator fun <State, Action, Environment> invoke(
-            reducer: ReducerFn<State, Action, Environment>
+            reducer: (State, Action, Environment) -> Result<State, Action>
         ): Reducer<State, Action, Environment> = Reducer(reducer)
 
         fun <State, Action, Environment> combine(vararg reducers: Reducer<State, Action, Environment>) =
             Reducer<State, Action, Environment> { value, action, environment ->
-                reducers.fold(ReducerResult(value, Effect.none())) { result, reducer ->
+                reducers.fold(Result(value, Effect.none())) { result, reducer ->
                     val (currentValue, currentEffect) = result
                     val (newValue, newEffect) = reducer.run(currentValue, action, environment)
                     currentEffect.merge(newEffect)
-                    ReducerResult(newValue, currentEffect)
+                    Result(newValue, currentEffect)
                 }
             }
     }
@@ -31,7 +28,7 @@ class Reducer<State, Action, Environment>(
         state: State,
         action: Action,
         environment: Environment
-    ): ReducerResult<State, Action> = reducer(state, action, environment)
+    ): Result<State, Action> = reducer(state, action, environment)
 
     fun combine(other: Reducer<State, Action, Environment>) = combine(this, other)
 
@@ -42,14 +39,14 @@ class Reducer<State, Action, Environment>(
     ): Reducer<GlobalState, GlobalAction, GlobalEnvironment> =
         Reducer { globalState, globalAction, globalEnvironment ->
             toLocalAction.getOption(globalAction).fold(
-                { ReducerResult(globalState, Effect.none()) },
+                { Result(globalState, Effect.none()) },
                 { localAction ->
                     val (state, effect) = reducer(
                         toLocalState.get(globalState),
                         localAction,
                         toLocalEnvironment(globalEnvironment)
                     )
-                    ReducerResult(
+                    Result(
                         toLocalState.set(globalState, state),
                         effect.map(toLocalAction::reverseGet)
                     )
@@ -66,19 +63,19 @@ class Reducer<State, Action, Environment>(
     ): Reducer<GlobalState, GlobalAction, GlobalEnvironment> =
         Reducer { globalState, globalAction, globalEnvironment ->
             toLocalAction.getOption(globalAction).fold(
-                { ReducerResult(globalState, Effect.none()) },
+                { Result(globalState, Effect.none()) },
                 { (id, localAction) ->
                     val localState = toLocalState.get(globalState)
                     val index = localState.indexOfFirst { findById.get(it) == id }
                     if (index < 0) {
-                        ReducerResult(globalState, Effect.none())
+                        Result(globalState, Effect.none())
                     } else {
                         val (state, effect) = reducer(
                             localState[index],
                             localAction,
                             toLocalEnvironment(globalEnvironment)
                         )
-                        ReducerResult(
+                        Result(
                             toLocalState.set(globalState, localState.update(index, state)),
                             effect.map { toLocalAction.reverseGet(id to localAction) }
                         )
@@ -89,7 +86,7 @@ class Reducer<State, Action, Environment>(
 
     fun optional(): Reducer<State?, Action, Environment> = Reducer { state, action, environment ->
         if (state == null) {
-            ReducerResult(state, Effect.none())
+            Result(state, Effect.none())
         } else {
             reducer(state, action, environment)
         }
