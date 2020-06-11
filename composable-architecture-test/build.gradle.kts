2fb6d1ea238acfc2fa00727d7ad1d061ad60c618
Gyula Voros <gyula@makery.co>
2020-06-11 09:43:49 +0200

    refact: remove Moshi as a dependency

--- composable-architecture-test/build.gradle.kts
+++ composable-architecture-test/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
 }
 
 dependencies {
-    implementation("com.squareup.moshi:moshi-kotlin:1.9.2")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.72")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.3.7")
     implementation(project(":composable-architecture"))
